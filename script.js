0d961181ea75875270b9846d009a1f41cff1d27a
5166-gamez <5166games@gmail.com>
2026-03-30 02:00:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// test line to see if its actually building IT SBUILDING BUT FOR SOME REASON THIS IS SYNTAX ERRORTING WTF
 const content = document.getElementById("content");
 
 function updateHeader(section) {
